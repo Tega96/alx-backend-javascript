28747ce04240e7840b816c5d59cd934b23c93162
Tega96 <otiteotega@gmail.com>
2024-10-13 15:03:22 +0000

    block scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default function taskBlock(trueOrFalse) {
+	let task;
+	let task2;
+
+	task = false;
+	task2 = true;
+
+	if (trueOrFalse) {
+		let task = true;
+		let task2 = false;
+		}
+
+	  return [task, task2];
+}
