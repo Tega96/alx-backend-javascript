0777838df2c0014b1ace69a753f8b80a510bd500
Tega96 <otiteotega@gmail.com>
2024-10-13 16:20:44 +0000

    blocked scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-	let task = false;
-	let task2 = true;
-/* eslint-disable-line no-unused-vars */
+	const task = false;
+	const task2 = true;
+	
 	if (trueOrFalse) {
-		let task = true;
-		let task2 = false;
+/* eslint-disable-line no-unused-vars */
+		const task = true;
+		const task2 = false;
+/* eslint-enable-line no-unused-vars */
 		}
-/* eslint-enable-line nounused-vars */
 	return [task, task2];
 }
