536dcebd58cbd2e9ccf2200f1386d6b1f29de7dd
Tega96 <otiteotega@gmail.com>
2024-10-13 15:54:59 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,9 +4,9 @@ export default function taskBlock(trueOrFalse) {
 	let task2 = true;
 
 	if (trueOrFalse) {
-		let task = true;
-		let task2 = false;
+		task = true;
+		task2 = false;
 		}
 
-	  return [task, task2];
+	return [task, task2];
 }
