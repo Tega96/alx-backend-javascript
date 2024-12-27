178451e31e307e106894c046e4c1ece3b125d3be
Tega96 <otiteotega@gmail.com>
2024-12-27 06:52:07 +0000

    using arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,8 @@
-export default function getNeighborhoodsList() {
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union square'];
+export default const getNeighborhoodsList = () => {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = (newNeighborhood) => {
+  this.addNeighborhood = const add = newNeighborhood => {
     self.sanFranciscoNeigborhoods.push(newNeighborhood);
     return self.sanFranciscoNeigbourhoods;
   };
