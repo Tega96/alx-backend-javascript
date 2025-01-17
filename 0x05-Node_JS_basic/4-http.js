50bedd944bbe6531590a4e35cf443e18b70d526d
Tega96 <otiteotega@gmail.com>
2025-01-17 02:02:50 +0000

    createe small server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
-const http = require('http');
+const { createServer } = require('node:http');
 
 const host = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello ALX!');
