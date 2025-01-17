f1661f6aab188864f18cbfcc70c000b1986c8edf
Tega96 <otiteotega@gmail.com>
2025-01-17 00:38:13 +0000

    creating http server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello ALX!');
+});
+
+app.listen(port, host, () => {});
+
+module.exports = app;
